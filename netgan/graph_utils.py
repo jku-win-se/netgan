f19ebcda35307cd8e38e3bd1759f49ce68584b35
AbbasRahimi <Abbasrahimi71@gmail.com>
2021-06-16 07:56:46 +0430

    changed data set for test a simple example

--- netgan/graph_utils.py
+++ netgan/graph_utils.py
@@ -40,16 +40,15 @@ def graph_similarity(sparse_matrix1, sparse_matrix2, r0, r1, i_timeout):
     sparse_matrix = sp.csr_matrix(sparse_matrix2)
     g2 = nx.from_scipy_sparse_matrix(sparse_matrix)
 
-    paths, cost = nx.optimal_edit_paths(g1, g2)
-    print("Paths: ", len(paths))
-    print("Graph edit distance: ", cost)
-    for v in nx.optimize_graph_edit_distance(g1, g2):
-        minv = v
-    print("Minimum cost of edit: ", minv)
     print("Generated graph is connected? ", nx.is_connected(g2))
     print("g1 and g2 are isomorphic? ", nx.could_be_isomorphic(g1, g2))
 
-    # edit_dis = nx.graph_edit_distance(g1, g2, roots=(r0, r1), timeout=i_timeout)
-    # print("Edit distance between g1 and g2: ", edit_dis)
+    edit_dis = nx.graph_edit_distance(g1, g2, roots=(r0, r1), timeout=i_timeout)
+    print("Edit distance between g1 and g2: ", edit_dis)
 
-
+    # paths, cost = nx.optimal_edit_paths(g1, g2)
+    # print("Paths: ", len(paths))
+    # print("Graph edit distance: ", cost)
+    # for v in nx.optimize_graph_edit_distance(g1, g2):
+    #     minv = v
+    # print("Minimum cost of edit: ", minv)
